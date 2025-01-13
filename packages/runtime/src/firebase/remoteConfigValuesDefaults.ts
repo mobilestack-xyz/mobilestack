1fb9abfeac859bbb590f8e972f2d2ef8b6236bd4
Jean Regisser <jean.regisser@gmail.com>
2025-01-13 11:31:17 +0100

    Avoid repetition

--- packages/runtime/src/firebase/remoteConfigValuesDefaults.ts
+++ packages/runtime/src/firebase/remoteConfigValuesDefaults.ts
@@ -12,44 +12,25 @@ export const REMOTE_CONFIG_VALUES_DEFAULTS: Omit<
   sentryNetworkErrors: string
   dappListApiUrl: string
   celoNews: string
-} = isE2EEnv
-  ? {
-      inviteRewardsVersion: 'none',
-      walletConnectV2Enabled: true,
-      pincodeUseExpandedBlocklist: true,
-      logPhoneNumberTypeEnabled: false,
-      allowOtaTranslations: false,
-      sentryTracesSampleRate: 0.2,
-      sentryNetworkErrors: '',
-      dappListApiUrl: 'https://us-central1-celo-mobile-alfajores.cloudfunctions.net/dappList',
-      maxNumRecentDapps: 4,
-      dappsWebViewEnabled: true,
-      fiatConnectCashInEnabled: false,
-      fiatConnectCashOutEnabled: true,
-      coinbasePayEnabled: false,
-      showSwapMenuInDrawerMenu: false,
-      maxSwapSlippagePercentage: 2,
-      networkTimeoutSeconds: 30,
-      celoNews: JSON.stringify({} as RemoteConfigValues['celoNews']),
-      priceImpactWarningThreshold: 0.04,
-    }
-  : {
-      inviteRewardsVersion: 'none',
-      walletConnectV2Enabled: true,
-      pincodeUseExpandedBlocklist: false,
-      logPhoneNumberTypeEnabled: false,
-      allowOtaTranslations: false,
-      sentryTracesSampleRate: DEFAULT_SENTRY_TRACES_SAMPLE_RATE,
-      sentryNetworkErrors: DEFAULT_SENTRY_NETWORK_ERRORS.join(','),
-      maxNumRecentDapps: 0,
-      dappsWebViewEnabled: false,
-      dappListApiUrl: '',
-      fiatConnectCashInEnabled: false,
-      fiatConnectCashOutEnabled: false,
-      coinbasePayEnabled: false,
-      showSwapMenuInDrawerMenu: false,
-      maxSwapSlippagePercentage: 2,
-      networkTimeoutSeconds: 30,
-      celoNews: JSON.stringify({} as RemoteConfigValues['celoNews']),
-      priceImpactWarningThreshold: 0.04,
-    }
+} = {
+  inviteRewardsVersion: 'none',
+  walletConnectV2Enabled: true,
+  pincodeUseExpandedBlocklist: isE2EEnv ? true : false,
+  logPhoneNumberTypeEnabled: false,
+  allowOtaTranslations: false,
+  sentryTracesSampleRate: DEFAULT_SENTRY_TRACES_SAMPLE_RATE,
+  sentryNetworkErrors: DEFAULT_SENTRY_NETWORK_ERRORS.join(','),
+  maxNumRecentDapps: isE2EEnv ? 4 : 0,
+  dappsWebViewEnabled: isE2EEnv ? true : false,
+  dappListApiUrl: isE2EEnv
+    ? 'https://us-central1-celo-mobile-alfajores.cloudfunctions.net/dappList'
+    : '',
+  fiatConnectCashInEnabled: false,
+  fiatConnectCashOutEnabled: isE2EEnv ? true : false,
+  coinbasePayEnabled: false,
+  showSwapMenuInDrawerMenu: false,
+  maxSwapSlippagePercentage: 2,
+  networkTimeoutSeconds: 30,
+  celoNews: JSON.stringify({} as RemoteConfigValues['celoNews']),
+  priceImpactWarningThreshold: 0.04,
+}
